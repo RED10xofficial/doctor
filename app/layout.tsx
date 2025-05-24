f23520f072c013ea67a5ffbf45d34e5b82c59e4a
MaHi <mahesh30795@gmail.com>
2025-05-24 21:36:21 +0530

    feat(scroll-to-top): add ScrollToTop component to reset scroll position on route change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import ScrollToTop from "./components/ScrollToTop";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -21,10 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={poppins.className}>
-      <body className={`${poppins.variable} antialiased `}>
-        
+      <body className={`${poppins.variable} antialiased`}>
+        <ScrollToTop />
         {children}
-        
       </body>
     </html>
   );
