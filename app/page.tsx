ea52ba1522518af904bec807014cca1a9c8302f7
red10xofficial@gmail.com <red10xofficial@gmail.com>
2025-05-25 10:34:38 +0530

    ui cha nges

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,5 @@
-import ProfileSidebar from "./components/profile";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen bg-gray-100">
-      {/* @ts-expect-error Async Server Component */}
-      <ProfileSidebar />
-      <div className="flex-1 p-8">
-        <h1 className="text-2xl font-bold text-gray-800">Main Content Area</h1>
-        <p className="text-gray-600 mt-4">
-          The profile sidebar is now implemented and ready to use!
-        </p>
-      </div>
-    </div>
-  );
+  return redirect("/home");
 }
